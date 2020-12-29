086239d7b178ca9d1a6165516c640974f117d5cb
user160244980349 <mkuznetsov7991@gmail.com>
2020-12-29 20:47:24 +0300

    Initital

---
+++ readme.md
@@ -0,0 +1,2 @@
+This is the frontend part for php-engine
+========================================
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: path.resolve(__dirname, 'js/index.js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [{ 
+                    loader: 'style-loader' 
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../public/js'),
+    },
+};
